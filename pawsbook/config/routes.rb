2643b111e0eb43c53c8306883686585a71503e83
abnadi <abnadi@me.com>
2016-06-11 13:01:00 +0200

    new 0002

--- pawsbook/config/routes.rb
+++ pawsbook/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
- #def
+
+  get 'users' => 'user#index'
+
+  root 'user#index'
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,6 +60,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  #end
-  root 'application#hello'
 end
