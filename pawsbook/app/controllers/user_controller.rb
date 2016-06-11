a02988c03d7670383f9aaef3bd114818792e1f83
ramonavic <ramon.hofmeister@gmail.com>
2016-06-11 12:09:26 +0200

    Merged user controller

--- pawsbook/app/controllers/user_controller.rb
+++ pawsbook/app/controllers/user_controller.rb
@@ -1,20 +1,4 @@
 class UserController < ApplicationController
-<<<<<<< HEAD
-  def index
-    @users = User.all
-  end
-
-  def show
-    #Shows a profile id
-    @user = User.find (params[:id])
-  end
-
-  def search
-    #Searches for a profile id and then shows it on search results page
-  end
-
-  
-=======
 
   def index
     @user = User.all
@@ -36,6 +20,5 @@ class UserController < ApplicationController
      end
      erb :'user/results'
     end
->>>>>>> 1d3dba0b1d9b6126a68d34ee95e46fa147a19e36
 
 end
