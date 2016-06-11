48b760ea00412af815edff6a789ed4e3c7a7abb8
ramonavic <ramon.hofmeister@gmail.com>
2016-06-11 16:03:56 +0200

    User images connected to user id

--- pawsbook/db/seeds.rb
+++ pawsbook/db/seeds.rb
@@ -5,9 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create( {name: "Cocoa Butter",     animal: "cat",  gender: "female",  picture: "13249723_992946357491472_860429745_n.jpg" })
-User.create( {name: "Flopsy Jefferson", animal: "cat",  gender: "male",    picture: "13249686_1559527621014954_1303573854_n.jpg"})
-User.create( {name: "June Bug",         animal: "cat",  gender: "male",    picture: "cat_1.jpg"})
-User.create( {name: "Hugo Deli",        animal: "dog",  gender: "male",    picture: "13258964_634559956706603_274810134_n.jpg"})
-User.create( {name: "Muff Van Helsin",  animal: "dog",  gender: "male",    picture: "13277753_1063492187064170_287976472_n.jpg"})
-User.create( {name: "Jose Ramos",       animal: "dog",  gender: "male",    picture: "4019634727_787c44a693_z.jpg"})
+User.delete_all
+Picture.delete_all
+cocoa_butter = User.create( {name: "Cocoa Butter",     animal: "cat",  gender: "female" })
+flopsy_jefferson = User.create( {name: "Flopsy Jefferson", animal: "cat",  gender: "male" })
+User.create( {name: "June Bug",         animal: "cat",  gender: "male" })
+User.create( {name: "Hugo Deli",        animal: "dog",  gender: "male" })
+User.create( {name: "Muff Van Helsin",  animal: "dog",  gender: "male" })
+User.create( {name: "Jose Ramos",       animal: "dog",  gender: "male" })
+
+pic = Picture.create(user: cocoa_butter)
+pic.name.store!(File.open(Rails.root.join('app', 'assets', 'images', 'cocoa_butter.jpg')))
+pic.save!
+
+pic = Picture.create(user: flopsy_jefferson)
+pic.name.store!(File.open(Rails.root.join('app', 'assets', 'images', 'flopsy.jpg')))
+pic.save!
