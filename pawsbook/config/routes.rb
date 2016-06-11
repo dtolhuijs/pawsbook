451d47643ae977e424f378a083cabecd0a9958ae
ramonavic <ramon.hofmeister@gmail.com>
2016-06-11 12:34:41 +0200

    Added user index

--- pawsbook/config/routes.rb
+++ pawsbook/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  get 'users' => 'user#index'
+
+  root 'user#index'
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
